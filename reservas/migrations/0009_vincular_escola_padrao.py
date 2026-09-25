89d089010d93ae834a73c9d6f53f8db743dbcb82
Antonny234 <lauanasantos696@gmail.com>
2026-09-25 15:32:26 -0300

    fix: alinha migracao de dados da escola padrao com a migracao 0008

--- reservas/migrations/0009_vincular_escola_padrao.py
+++ reservas/migrations/0009_vincular_escola_padrao.py
@@ -5,19 +5,18 @@ def popular_escola_padrao(apps, schema_editor):
     PerfilProfessor = apps.get_model('reservas', 'PerfilProfessor')
     PerfilAdm = apps.get_model('reservas', 'PerfilAdm')
 
-    # 1. Cria a escola atual no banco (ajuste o nome se desejar)
+    # Cria ou obtém a escola principal
     escola_padrao, _ = Escola.objects.get_or_create(
         nome="Escola Principal"
     )
 
-    # 2. Associa todos os professores e administradores existentes a essa escola
+    # Associa os professores e administradores existentes
     PerfilProfessor.objects.filter(escola__isnull=True).update(escola=escola_padrao)
     PerfilAdm.objects.filter(escola__isnull=True).update(escola=escola_padrao)
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        # Mantém a dependência gerada automaticamente pelo Django
         ('reservas', '0008_cargo_texto_livre'),
     ]
 
